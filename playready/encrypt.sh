a2096397c7ffa3ddb1c06a0135003fe638c64d05
Greg Rutz <g.rutz@cablelabs.com>
2014-01-23 11:12:16 -0700

    Updates to encryption to use latest MP4Box fixes.
    Added line to script to convert playready XML to UTF-16 encoded before
    it is used for creating the PSSH.

--- playready/encrypt.sh
+++ playready/encrypt.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 
-root_dir="/home/grutz/Projects/CableLabs/EME-HSE/MP4"
-gpac_bin_dir="$root_dir/gpac/gpac/bin/gcc"
+root_dir="/home/grutz/Projects/CableLabs"
+gpac_bin_dir="$root_dir/gpac/bin/gcc"
 export LD_LIBRARY_PATH=$gpac_bin_dir
 
 pushd .
 
-cd $root_dir/playready
-$gpac_bin_dir/MP4Box -crypt playready_cenc.xml $root_dir/content/bbb_720p_h264-2Mb-high-3.1_aac-lc_track1_dashinit.mp4 -out $root_dir/content/bbb_720p_h264-2Mb-high-3.1_aac-lc_track1_dashinit_enc.mp4
-$gpac_bin_dir/MP4Box -crypt playready_cenc.xml $root_dir/content/bbb_720p_h264-3Mb-high-3.1_aac-lc_track1_dashinit.mp4 -out $root_dir/content/bbb_720p_h264-3Mb-high-3.1_aac-lc_track1_dashinit_enc.mp4
+cd $root_dir/mse-eme/mp4/playready
+
+# WRM data must be UTF-16 encoded as per MS spec
+iconv -f UTF-8 -t UTF-16LE playready_wrm.xml > playready_wrm_utf16.xml 
+
+$gpac_bin_dir/MP4Box -crypt playready_cenc.xml $root_dir/mse-eme/mp4/bbb_720p_h264-2Mb-high-3.1_aac-lc.mp4 -out $root_dir/mse-eme/mp4/bbb_720p_h264-2Mb-high-3.1_aac-lc_enc.mp4
+$gpac_bin_dir/MP4Box -crypt playready_cenc.xml $root_dir/mse-eme/mp4/bbb_720p_h264-3Mb-high-3.1_aac-lc.mp4 -out $root_dir/mse-eme/mp4/bbb_720p_h264-3Mb-high-3.1_aac-lc_enc.mp4
 
 popd
