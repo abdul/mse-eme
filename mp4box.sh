a6bb83a3e97743db0dc671f7d3254943b2e99c6f
Greg Rutz <g.rutz@cablelabs.com>
2014-03-04 09:55:08 -0700

    Latest updates to encryption/packaging scripts

--- mp4box.sh
+++ mp4box.sh
@@ -1,15 +1,30 @@
 #!/bin/bash
 
-root_dir="/home/grutz/Projects/CableLabs"
-gpac_bin_dir="$root_dir/gpac/bin/gcc"
+if [ -z $GPAC_ROOT_DIR ]; then
+  echo "Must set GPAC_ROOT_DIR!"
+  exit
+fi
+if [ -z $CONTENT_DIR ]; then
+  echo "Must set CONTENT_DIR!"
+  exit
+fi
+
+content_root_dir=$CONTENT_DIR
+gpac_bin_dir="$GPAC_ROOT_DIR/bin/gcc"
 export LD_LIBRARY_PATH=$gpac_bin_dir
 
 pushd .
 
-$gpac_bin_dir/MP4Box -dash 10000 -rap -bs-switching no -no-split-sg-boxes \
-  -profile onDemand -out bbb_720p_h264.mpd \
-  $root_dir/mse-eme/mp4/bbb_720p_h264-2Mb-high-3.1_aac-lc_enc.mp4#video:id=2Mb \
-  $root_dir/mse-eme/mp4/bbb_720p_h264-2Mb-high-3.1_aac-lc_enc.mp4#audio:id=audio \
-  $root_dir/mse-eme/mp4/bbb_720p_h264-3Mb-high-3.1_aac-lc_enc.mp4#video:id=3Mb 
+$gpac_bin_dir/MP4Box -dash 10000 -rap -bs-switching no -sample-groups-traf \
+  -profile onDemand -out bbb_720p_h264_enc.mpd \
+  $content_root_dir/bbb_720p_h264-2Mb-high-3.1_aac-lc_enc.mp4#video:id=2Mb \
+  $content_root_dir/bbb_720p_h264-2Mb-high-3.1_aac-lc_enc.mp4#audio:id=audio \
+  $content_root_dir/bbb_720p_h264-3Mb-high-3.1_aac-lc_enc.mp4#video:id=3Mb 
+
+#$gpac_bin_dir/MP4Box -dash 10000 -rap -bs-switching no -sample-groups-traf \
+#  -profile onDemand -out bbb_720p_h264.mpd \
+#  $content_root_dir/bbb_720p_h264-2Mb-high-3.1_aac-lc.mp4#video:id=2Mb \
+#  $content_root_dir/bbb_720p_h264-2Mb-high-3.1_aac-lc.mp4#audio:id=audio \
+#  $content_root_dir/bbb_720p_h264-3Mb-high-3.1_aac-lc.mp4#video:id=3Mb 
 
 popd
--- playready/encrypt.sh
+++ playready/encrypt.sh
@@ -1,21 +1,28 @@
 #!/bin/bash
 
-root_dir="/home/grutz/Projects/CableLabs"
-gpac_bin_dir="$root_dir/gpac/bin/gcc"
+if [ -z $GPAC_ROOT_DIR ]; then
+  echo "Must set GPAC_ROOT_DIR!"
+  exit
+fi
+if [ -z $CONTENT_DIR ]; then
+  echo "Must set CONTENT_DIR!"
+  exit
+fi
+
+content_root_dir=$CONTENT_DIR
+gpac_bin_dir="$GPAC_ROOT_DIR/bin/gcc"
 export LD_LIBRARY_PATH=$gpac_bin_dir
 
 pushd .
-
-cd $root_dir/mse-eme/mp4/playready
 
 # WRM data must be UTF-16 encoded as per MS spec
 iconv -f UTF-8 -t UTF-16LE playready_wrm_v4100.xml > playready_wrm_v4100_utf16.xml 
 iconv -f UTF-8 -t UTF-16LE playready_wrm_v4000.xml > playready_wrm_v4000_utf16.xml 
 
-#$gpac_bin_dir/MP4Box -crypt playready_cenc_v4000.xml $root_dir/mse-eme/mp4/bbb_720p_h264-2Mb-high-3.1_aac-lc.mp4 -out $root_dir/mse-eme/mp4/bbb_720p_h264-2Mb-high-3.1_aac-lc_enc.mp4
-#$gpac_bin_dir/MP4Box -crypt playready_cenc_v4000.xml $root_dir/mse-eme/mp4/bbb_720p_h264-3Mb-high-3.1_aac-lc.mp4 -out $root_dir/mse-eme/mp4/bbb_720p_h264-3Mb-high-3.1_aac-lc_enc.mp4
+$gpac_bin_dir/MP4Box -crypt playready_cenc_v4000.xml $content_root_dir/bbb_720p_h264-2Mb-high-3.1_aac-lc.mp4 -out $content_root_dir/bbb_720p_h264-2Mb-high-3.1_aac-lc_enc.mp4
+$gpac_bin_dir/MP4Box -crypt playready_cenc_v4000.xml $content_root_dir/bbb_720p_h264-3Mb-high-3.1_aac-lc.mp4 -out $content_root_dir/bbb_720p_h264-3Mb-high-3.1_aac-lc_enc.mp4
 
-$gpac_bin_dir/MP4Box -crypt playready_cenc_v4100.xml $root_dir/mse-eme/mp4/bbb_720p_h264-2Mb-high-3.1_aac-lc.mp4 -out $root_dir/mse-eme/mp4/bbb_720p_h264-2Mb-high-3.1_aac-lc_enc.mp4
-$gpac_bin_dir/MP4Box -crypt playready_cenc_v4100.xml $root_dir/mse-eme/mp4/bbb_720p_h264-3Mb-high-3.1_aac-lc.mp4 -out $root_dir/mse-eme/mp4/bbb_720p_h264-3Mb-high-3.1_aac-lc_enc.mp4
+#$gpac_bin_dir/MP4Box -crypt playready_cenc_v4100.xml $content_root_dir/bbb_720p_h264-2Mb-high-3.1_aac-lc.mp4 -out $content_root_dir/bbb_720p_h264-2Mb-high-3.1_aac-lc_enc.mp4
+#$gpac_bin_dir/MP4Box -crypt playready_cenc_v4100.xml $content_root_dir/bbb_720p_h264-3Mb-high-3.1_aac-lc.mp4 -out $content_root_dir/bbb_720p_h264-3Mb-high-3.1_aac-lc_enc.mp4
 
 popd
