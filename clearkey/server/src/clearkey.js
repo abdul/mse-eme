0222e6976b9053790160397aab3c07a31a2551dd
Greg Rutz <g.rutz@cablelabs.com>
2014-03-11 10:28:54 -0600

    Latest clearkey changes

--- clearkey/server/src/clearkey.js
+++ clearkey/server/src/clearkey.js
@@ -1,10 +1,11 @@
 
 var https = require('https');
+var http = require('http');
 var fs = require('fs');
 
 var keys = {
-    10000000100010001000000000000001: 0x3,
-    10000000100010001000000000000002: 0x4
+    10000000100010001000000000000001: '04714BD8D7E1F3815FC47D0A834F0E17',
+    10000000100010001000000000000002: '04714BD8D7E1F3815FC47D0A834F0E17'
 };
 
 var options = {
@@ -12,9 +13,38 @@ var options = {
     cert: fs.readFileSync('security/cl_clearkey-cert.pem')
 };
 
+var addCORSHeaders = function(res, length) {
+    res.writeHeader(200, {
+        "Content-Length": length,
+        "Content-Type": 'application/json',
+        "Access-Control-Allow-Origin": '*',
+        "Access-Control-Allow-Methods": 'GET, PUT, POST, DELETE, OPTIONS',
+        "Access-Control-Allow-Headers": 'Content-Type, Authorization, Content-Length, X-Requested-Width'});
+};
+
 https.createServer(options, function(req, res) {
-    res.writeHead(200);
+    addCORSHeaders(res);
     res.end("hello world\n");
 
 }).listen(8585);
+
+http.createServer(function(req, res) {
+    var requested_kid = "10000000100010001000000000000001";
+    var jwk = {
+        kty: "oct",
+        alg: "A128GCM",
+        kid: requested_kid,
+        k: keys[requested_kid]
+    };
+    var jwk_array = [];
+    jwk_array.push(jwk);
+    var response = {
+        keys: jwk_array
+    };
+    var json_str_response = JSON.stringify(response);
+    addCORSHeaders(res, json_str_response.length);
+    res.write(json_str_response);
+    res.end();
+
+}).listen(8584);
 
