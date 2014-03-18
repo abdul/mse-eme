de7c6c29f5c0a80a15a65b789aeb5436d4a90e11
Greg Rutz <g.rutz@cablelabs.com>
2014-03-18 16:12:56 -0600

    Update non-encryption script to use GPAC env vars

--- mp4box_noenc.sh
+++ mp4box_noenc.sh
@@ -1,12 +1,20 @@
 #!/bin/bash
 
+if [ -z $GPAC_ROOT_DIR ]; then
+  echo "Must set GPAC_ROOT_DIR!"
+  exit
+fi
+if [ -z $CONTENT_DIR ]; then
+  echo "Must set CONTENT_DIR!"
+  exit
+fi
+
 root_dir="/home/grutz/Projects/CableLabs"
 gpac_bin_dir="$root_dir/gpac/bin/gcc"
 export LD_LIBRARY_PATH=$gpac_bin_dir
 
 pushd .
 
-cd $root_dir/content
 $gpac_bin_dir/MP4Box -dash 10000 -rap -bs-switching no -profile onDemand -out bbb_720p_h264_noenc.mpd \
   $root_dir/mse-eme/mp4/bbb_720p_h264-2Mb-high-3.1_aac-lc.mp4#video:id=2Mb \
   $root_dir/mse-eme/mp4/bbb_720p_h264-2Mb-high-3.1_aac-lc.mp4#audio:id=audio \
