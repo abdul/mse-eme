47951bbf6a53c37efb0e809effed4019c0dd1aef
Greg Rutz <g.rutz@cablelabs.com>
2014-03-21 15:44:53 -0600

    More automation to encryption script
    Automated:
     - removal of formatting in the PlayReady Header XML files.
     - insertion of playready header sizes
    
    Fixed a typo in the header XML tag name

--- playready/encrypt.sh
+++ playready/encrypt.sh
@@ -15,14 +15,21 @@ export LD_LIBRARY_PATH=$gpac_bin_dir
 
 pushd .
 
-# WRM data must be UTF-16 encoded as per MS spec
-iconv -f UTF-8 -t UTF-16LE playready_wrm_v4100.xml > playready_wrm_v4100_utf16.xml 
-iconv -f UTF-8 -t UTF-16LE playready_wrm_v4000.xml > playready_wrm_v4000_utf16.xml 
+version=4000
+#version=4100
 
-#$gpac_bin_dir/MP4Box -crypt playready_cenc_v4000.xml $content_root_dir/bbb_720p_h264-2Mb-high-3.1_aac-lc.mp4 -out $content_root_dir/bbb_720p_h264-2Mb-high-3.1_aac-lc_enc.mp4
-#$gpac_bin_dir/MP4Box -crypt playready_cenc_v4000.xml $content_root_dir/bbb_720p_h264-3Mb-high-3.1_aac-lc.mp4 -out $content_root_dir/bbb_720p_h264-3Mb-high-3.1_aac-lc_enc.mp4
+# Remove all leading whitespace and then remove all end of line characters
+# Then convert to UTF-16LE
+sed 's/^[ \t]*//' playready_wrm_v${version}.xml | tr -d '\r\n' | iconv -f UTF-8 -t UTF-16LE > wrm_utf16.xml
 
-$gpac_bin_dir/MP4Box -crypt playready_cenc_v4100.xml $content_root_dir/bbb_720p_h264-2Mb-high-3.1_aac-lc.mp4 -out $content_root_dir/bbb_720p_h264-2Mb-high-3.1_aac-lc_enc.mp4
-$gpac_bin_dir/MP4Box -crypt playready_cenc_v4100.xml $content_root_dir/bbb_720p_h264-3Mb-high-3.1_aac-lc.mp4 -out $content_root_dir/bbb_720p_h264-3Mb-high-3.1_aac-lc_enc.mp4
+# Grab file size and insert into our crypt file template
+size=`du -b wrm_utf16.xml | awk '{print $1}'`
+total_size=`expr $size + 10`
+sed -e "s/___WRM_SIZE___/$size/" -e "s/___PRHO_SIZE___/$total_size/" playready_cenc.xml > cryptfile
+$gpac_bin_dir/MP4Box -crypt cryptfile $content_root_dir/bbb_720p_h264-2Mb-high-3.1_aac-lc.mp4 -out $content_root_dir/bbb_720p_h264-2Mb-high-3.1_aac-lc_enc.mp4
+$gpac_bin_dir/MP4Box -crypt cryptfile $content_root_dir/bbb_720p_h264-3Mb-high-3.1_aac-lc.mp4 -out $content_root_dir/bbb_720p_h264-3Mb-high-3.1_aac-lc_enc.mp4
+
+rm cryptfile
+rm wrm_utf16.xml
 
 popd
