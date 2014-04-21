79f1293ea17c8bd42d5b42bdf150ae5d17888471
Greg Rutz <g.rutz@cablelabs.com>
2014-04-21 14:51:22 -0600

    Remove use of separate URL file for clearkey
    Don't need a separate URL file for clearkey.  We can just add
    our string right into the MP4Box cryptfile

--- clearkey/encrypt.sh
+++ clearkey/encrypt.sh
@@ -15,20 +15,7 @@ export LD_LIBRARY_PATH=$gpac_bin_dir
 
 pushd .
 
-ck_cleanfile="clearkey_clean_url.txt"
-ck_cryptfile="ck_cryptfile"
-
-# Remove all leading whitespace and all end of line characters
-sed 's/^[ \t]*//' clearkey_url.txt | tr -d '\r\n' > $ck_cleanfile
-
-# Grab file size and insert into our crypt file template
-size=`du -b $ck_cleanfile | awk '{print $1}'`
-
-sed -e "s/___CK_URL_SIZE___/$size/" -e "s/___CK_URL_FILE___/$ck_cleanfile/" clearkey_cenc.xml > $ck_cryptfile
-
-$gpac_bin_dir/MP4Box -crypt $ck_cryptfile $content_root_dir/bbb_720p_h264-2Mb-high-3.1_aac-lc.mp4 -out $content_root_dir/bbb_720p_h264-2Mb-high-3.1_aac-lc_enc.mp4
-$gpac_bin_dir/MP4Box -crypt $ck_cryptfile $content_root_dir/bbb_720p_h264-3Mb-high-3.1_aac-lc.mp4 -out $content_root_dir/bbb_720p_h264-3Mb-high-3.1_aac-lc_enc.mp4
-
-rm $ck_cryptfile $ck_cleanfile
+$gpac_bin_dir/MP4Box -crypt clearkey_cenc.xml $content_root_dir/bbb_720p_h264-2Mb-high-3.1_aac-lc.mp4 -out $content_root_dir/bbb_720p_h264-2Mb-high-3.1_aac-lc_enc.mp4
+$gpac_bin_dir/MP4Box -crypt clearkey_cenc.xml $content_root_dir/bbb_720p_h264-3Mb-high-3.1_aac-lc.mp4 -out $content_root_dir/bbb_720p_h264-3Mb-high-3.1_aac-lc_enc.mp4
 
 popd
