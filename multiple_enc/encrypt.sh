127b89967b05381951895231794aa79fc095e248
Greg Rutz <g.rutz@cablelabs.com>
2014-05-01 13:18:31 -0600

    Missed this file on my last commit

--- multiple_enc/encrypt.sh
+++ multiple_enc/encrypt.sh
@@ -1,24 +1,49 @@
 #!/bin/bash
 
-if [ -z $GPAC_ROOT_DIR ]; then
-  echo "Must set GPAC_ROOT_DIR!"
-  exit
-fi
-if [ -z $CONTENT_DIR ]; then
-  echo "Must set CONTENT_DIR!"
-  exit
-fi
+function usage {
+  echo ""
+  echo "PlayReady Encryption Script"
+  echo "usage:"
+  echo "   encrypt.sh -o <output_directory> -v [4000|4100] [INPUT_FILE]..."
+}
 
-if [ -z $1 ]; then
-  echo "Must provide version argument (either 4000 or 4100)"
-  exit
+while getopts ":o:v:" opt; do
+  case $opt in
+    o)
+      output_dir=$OPTARG
+      ;;
+    v)
+      pr_wrm_version=$OPTARG
+      ;;
+    \?)
+      echo "Invalid option: -$OPTARG" >&2
+      usage
+      exit 1
+      ;;
+    :)
+      echo "Missing options argument for -$OPTARG" >&2
+      usage
+      exit 1
+      ;;
+  esac
+done
+shift $((OPTIND - 1))
+
+if [ -z $pr_wrm_version ]; then
+  echo "Must provide PlayReady version argument (either 4000 or 4100)"
+  usage
+  exit 1
 fi
-
-content_root_dir=$CONTENT_DIR
-gpac_bin_dir="$GPAC_ROOT_DIR/bin/gcc"
-export LD_LIBRARY_PATH=$gpac_bin_dir
-
-pushd .
+if [ -z $output_dir ]; then
+  echo "Must provide output directory for encrypted media files"
+  usage
+  exit 1
+fi
+if [ -z $@ ]; then
+  echo "No input media files specified!"
+  usage
+  exit 0
+fi
 
 cryptfile="cryptfile"
 pr_wrm_version=$1
@@ -31,19 +56,15 @@ sed 's/^[ \t]*//' ../playready/playready_wrm_v${pr_wrm_version}.xml | tr -d '\r\
 # Grab file size and insert into our crypt file template
 pr_wrm_size=`du -b $pr_utf16file | awk '{print $1}'`
 pr_total_size=`expr $size + 10`
-
-# Grab file size and insert into our crypt file template
-size=`du -b $ck_cleanfile | awk '{print $1}'`
-
 sed -e "s/___WRM_SIZE___/$pr_wrm_size/" \
   -e "s/___PRHO_SIZE___/$pr_total_size/" \
   -e "s/___WRM_FILE___/$pr_utf16file/" cenc_pr_ck.xml > $cryptfile
-
-$gpac_bin_dir/MP4Box -crypt $cryptfile $content_root_dir/bbb_720p_h264-2Mb-high-3.1_aac-lc.mp4 -out $content_root_dir/bbb_720p_h264-2Mb-high-3.1_aac-lc_enc.mp4
-$gpac_bin_dir/MP4Box -crypt $cryptfile $content_root_dir/bbb_720p_h264-3Mb-high-3.1_aac-lc.mp4 -out $content_root_dir/bbb_720p_h264-3Mb-high-3.1_aac-lc_enc.mp4
+mkdir -p $output_dir
+for file in $@; do
+  MP4Box -crypt $pr_cryptfile $file -out $output_dir/`basename $file`
+done
 
 rm $pr_utf16file
 rm $cryptfile
 
-popd
 
